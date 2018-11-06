ca2f00f657dc7efb97720305913ca369da262c6e
Kari Piukka <kari.piukka@gmail.com>
2018-11-06 15:37:51 +0200

    Erilliset routes-tiedostot

--- backend/routes/kayttaja.js
+++ backend/routes/kayttaja.js
@@ -1,9 +1,30 @@
+const express = require('express');
+const app = express();
+
+// Käyttäjä-Router
+const kayttajaRouter = express.Router();
+app.use('/kayttaja', kayttajaRouter);
+
 // Luo käyttäjätunnus / rekisteröi
+kayttajaRouter.post('/', (req, res, next) => {
+    res.send('{"viesti": "Käyttäjä luotu"}');
+});
 
 // Katso omia tietoja
+kayttajaRouter.get('/:id', (req, res, next) => {
+    res.send('{"tiedot": "tähän"}');
+});
 
 // Muokkaa omia tietoja
+kayttajaRouter.put('/:id', (req, res, next) => {
+    res.send('{"viesti": "Käyttäjätiedot muutettu"}');
+});
 
 // Poista oma tili
+kayttajaRouter.delete('/:id', (req, res, next) => {
+    res.send('{"viesti": "Käyttäjä poistettu"}');
+});
 
-// Vaihda salasana
+// Vaihda salasana (onko omien tietojen muokkaus?)
+
+module.exports = kayttajaRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const polku = require('path');
 
-// bikes-kirjaston tuonti
+// ilmoitukset-kirjaston tuonti
 const ilmoitukset = require('./ilmoitukset.js');
 
 // Portin määritys
@@ -14,33 +14,14 @@ app.listen(portti, () => {
     console.log(`Palvelin kuuntelee porttia ${portti}`);
 });
 
+// Routes
+const ilmoitusRouter = require('./routes/ilmoitus.js');
+const kayttajaRouter = require('./routes/kayttaja.js');
+
+app.use('/ilmoitukset', ilmoitusRouter);
+app.use('/kayttaja', kayttajaRouter);
+
 // Perus-get
 app.get('/', (req, res, next) => {
     res.sendFile(polku.join(__dirname+'/testisivu.html'));
 });
-
-// Get ilmoitukset
-app.get('/ilmoitukset', (req, res, next) => {
-    res.send(ilmoitukset);
-});
-
-// Get ilmoitus:id
-app.get('/ilmoitus/:id', (req, res, next) => {
-    let id = req.params.id;
-    let ilmoitus = etsiIdlla(id);
-    if (ilmoitus) {
-        res.send(ilmoitus);
-    } else {
-        res.status(404).send(`{"virhe": "ilmoitusta ei löytynyt id:llä  ${id}"}`);
-    }
-});
-
-function etsiIdlla(id) {
-    let ilmoitus = null;
-    ilmoitukset.forEach(ilm => {
-        if (ilm.id == id) {
-            ilmoitus = ilm;
-        }
-    });
-    return ilmoitus;
-}
