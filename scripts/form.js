b216dae11cab1e1d37c63f9993038a73e9bf45a8
Olli-Pekka Vieno <hctarmo@gmail.com>
2018-11-07 16:33:22 +0200

    made a mtb menu on leave post page

--- scripts/form.js
+++ scripts/form.js
@@ -4,3 +4,29 @@ document.getElementById("logo").addEventListener("click", function() {
   window.location = "http://127.0.0.1:5500/index.html";
   console.log("testi");
 });
+function myFunction() {
+  var x = document.getElementById("mySelect").value;
+
+  if (x === "mtb") {
+    console.log("toimii");
+    let div = document.querySelector(".form");
+    let element = document.createElement("select");
+    let firstItem = document.createElement("option");
+    let secondItem = document.createElement("option");
+    let thirdItem = document.createElement("option");
+    let fourthItem = document.createElement("option");
+    let noSuspesionNode = document.createTextNode("Täysjäykkä");
+    let frontSuspensionNode = document.createTextNode("Etujousto");
+    let fullSuspensionNode = document.createTextNode("Täysjousto");
+    let fatbikeNode = document.createTextNode("Fatbike");
+    firstItem.appendChild(noSuspesionNode);
+    secondItem.appendChild(frontSuspensionNode);
+    thirdItem.appendChild(fullSuspensionNode);
+    fourthItem.appendChild(fatbikeNode);
+    element.appendChild(firstItem);
+    element.appendChild(secondItem);
+    element.appendChild(thirdItem);
+    element.appendChild(fourthItem);
+    div.appendChild(element);
+  }
+}
