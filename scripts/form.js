c35dde7f1ae29ff90577a19ce69469a044ad0a4e
Olli-Pekka Vieno <hctarmo@gmail.com>
2018-11-07 11:17:42 +0200

    git harjoittelua

---
+++ scripts/form.js
@@ -0,0 +1,6 @@
+console.log("testi");
+//Evenlistener to logo this apears in every page
+document.getElementById("logo").addEventListener("click", function() {
+  window.location = "http://127.0.0.1:5500/index.html";
+  console.log("testi");
+});
