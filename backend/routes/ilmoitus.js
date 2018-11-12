1e241ba02bb5a0bab43fc062780845d7d8d9ee60
John Doe <sami.rasouli@icloud.com>
2018-11-12 11:26:22 +0200

    testi

---
+++ backend/routes/ilmoitus.js
@@ -0,0 +1,53 @@
+// Moduulien tuonto
+const express = require('express');
+const app = express();
+const polku = require('path');
+//const ilmoitukset = require('../ilmoitukset.js');
+
+// Routerin luonti
+const ilmoitusRouter = express.Router();
+app.use('/ilmoitukset', ilmoitusRouter);
+
+// Lisää ilmoitus
+ilmoitusRouter.post('/', (req, res, next) => {
+    res.send({"viesti": "Ilmoitus lisätty"});
+});
+
+// Selaa ilmoituksia (hae kaikki ilmoitukset)
+ilmoitusRouter.get('/', (req, res, next) => {
+    res.send(ilmoitukset);
+});
+
+// Hae omat ilmoitukset
+
+// Hae yksittäinen ilmoitus
+ilmoitusRouter.get('/:id', (req, res, next) => {
+    let id = req.params.id;
+    let ilmoitus = etsiIdlla(id);
+    if (ilmoitus) {
+        res.send(ilmoitus);
+    } else {
+        res.status(404).send({"virhe": "ilmoitusta ei löytynyt id:llä  "+id+""});
+    }
+});
+
+// Poista oma ilmoitus
+ilmoitusRouter.delete('/:id', (req, res, next) => {
+    res.send({"viesti": "Ilmoitus poistettu"});
+});
+
+// Muokkaa ilmoitusta (laitetaanko?)
+
+
+// Funktiot
+function etsiIdlla(id) {
+    let ilmoitus = null;
+    ilmoitukset.forEach(ilm => {
+        if (ilm.id == id) {
+            ilmoitus = ilm;
+        }
+    });
+    return ilmoitus;
+}
+
+module.exports = ilmoitusRouter;
---
+++ backend/routes/kayttaja.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const app = express();
+const sql = require('mysql');
+
+const yhteys = sql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "fillarinetti"
+    }
+);
+
+// Käyttäjä-Router
+const kayttajaRouter = express.Router();
+app.use('/kayttaja', kayttajaRouter);
+
+// Luo käyttäjätunnus / rekisteröi
+kayttajaRouter.post('/', (req, res, next) => {
+    let user = req.query;
+    yhteys.query("insert into kayttaja values (?, ?, ?, ?, now(), now())", [user.username, user.password, user.sposti, user.puhnro] , (err, result, fields) => {
+        if (err){
+            res.send({"virhe": err.message});
+        } else {
+            res.send({"viesti": "Käyttäjä "+user.username+" lisätty"});
+        }
+    });
+});
+
+// Listaa kaikki käyttäjät
+kayttajaRouter.get('/', (req, res, next) => {
+    yhteys.query("select username, tunnusluotu from kayttaja", (err, result, fields) => {
+        let lista = result;
+        //console.log(lista);
+        if (err){
+            res.send({"virhe": err.message});
+        } else {
+            res.send(lista);
+        }
+    });
+});
+
+// Katso käyttäjän tietoja
+kayttajaRouter.get('/:username', (req, res, next) => {
+    // Haetaan kaikki 
+    let kayttajaTunnus = req.params.username;
+    yhteys.query("select * from kayttaja where username = ?", [kayttajaTunnus], (err, result, fields) => {
+        console.log(result);
+        if (err){
+            res.send({"virhe": err.message});
+        } else {
+            res.send(result[0]);
+        }
+    });
+});
+
+// Muokkaa omia tietoja
+kayttajaRouter.put('/:username', (req, res, next) => {
+    let kayttajaTunnus = req.params.username;
+    let user = req.query;
+
+    yhteys.query("update kayttaja set sposti = ?, puhnro = ? where username = ?", [user.sposti, user.puhnro, kayttajaTunnus], (err, result, fields) => {
+        console.log(result);
+        if (err){
+            res.send({"virhe": err.message});
+        } else {
+            res.send({ "viesti": "Käyttäjätiedot muutettu" });
+        }
+    });
+});
+
+// Poista oma tili
+kayttajaRouter.delete('/:username', (req, res, next) => {
+    let kayttajatunnus = req.params.username;
+    yhteys.query("delete from kayttaja where username = ?", [kayttajatunnus], (err, result, fields) => {
+        console.log(result);
+        if (err){
+            res.send({"virhe": err.message});
+        } else {
+            res.send({ "viesti": "Käyttäjä poistettu" });
+        }
+    });
+});
+
+// Vaihda salasana (onko omien tietojen muokkaus?)
+kayttajaRouter.put('/:username/salasana', (req, res, next) => {
+    let kayttajaTunnus = req.params.username;
+    let user = req.query;
+
+    yhteys.query("update kayttaja password = ? where username = ?", [user.password, kayttajaTunnus], (err, result, fields) => {
+        console.log(result);
+        if (err){
+            res.send({"virhe": err.message});
+        } else {
+            res.send({ "viesti": "Salasana muutettu" });
+        }
+    });
+});
+
+module.exports = kayttajaRouter;
