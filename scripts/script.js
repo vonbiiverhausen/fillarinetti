706d24f0dd50092a1c607b38f5476ab14a152c10
Sami-Rasouli <37326616+Sami-Rasouli@users.noreply.github.com>
2018-11-29 10:40:22 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,8 @@ document.getElementById("logo").addEventListener("click", function() {
   window.location = "http://127.0.0.1:5500/index.html";
   console.log("testi");
 });
-
-  document.getElementById("accurate_searchBtn")
+document
+  .getElementById("accurate_search")
   .addEventListener("click", function() {
     window.location = "http://127.0.0.1:5500/accurateSearch.html";
   });
-register = () => {
-  window.location = "http://127.0.0.1:5500/Registeri.html";
-};
